7a9fc2d2ef90492f2903094334a73573e16c95a9
Mysia22 <milkiway897@gmail.com>
2017-01-27 12:29:28 +0300

    at, insertAt, isEmpty, IndexOf..

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,7 +1,8 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {
+  
+  constructor() {
     this.length = 0;  
     this._head = new Node();
     this._tail = this._head;
@@ -23,20 +24,63 @@ class LinkedList {
     this.tail = function() {
       return this._tail.data;
     };
-
-    at(index) {}
-
-    insertAt(index, data) {}
-
-    isEmpty() {}
-
-    clear() {}
-
-    deleteAt(index) {}
-
-    reverse() {}
-
-    indexOf(data) {}
+    
+    this.at = function(index) {
+      const found = getNodeAt.call(this, index);
+      return (found === undefined) ? undefined : found.data;
+    };
+    
+    this.insertAt = function(index, data) {
+      let found = getNodeAt.call(this, index);
+      if (found === undefined) {
+        return this;
+      }
+      let newNode = new Node(data, found.prev, found);
+      if (found.prev !== null) {
+        found.prev.next = newNode;
+      }
+      ++this.length;
+      return this;
+    };
+    
+    this.isEmpty = function() {
+      return !this.length;
+    };
+    
+    this.clear = function() {
+      
+    };
+    
+    this.deleteAt = function(index) {
+    
+    };
+    
+    this.reverse = function() {
+      
+    };
+    
+    this.indexOf = function(data) {
+      let nextNode = this._head;
+      for (let i = 0; i < this.length; ++i) {
+        if (nextNode.data === data) {
+          return i;
+        }
+        nextNode = nextNode.next;
+      }
+      return -1;
+    };
+    
+    function getNodeAt(index) {
+      let nextNode = this._head;
+      for (let i = 0; i < this.length; ++i) {
+        if (i === index) {
+          return nextNode;
+        }
+        nextNode = nextNode.next;
+      }
+      return undefined;
+    }
+  }
 }
 
 module.exports = LinkedList;
